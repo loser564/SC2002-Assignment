6668f4cc1891d0c6bb585469f161a58b139ea577
Alicia Lam <66523331+loser564@users.noreply.github.com>
2023-11-10 16:28:52 +0800

    Some changes
    made some changes to organisation and camp file

--- src/basic/Camp.java
+++ src/basic/Camp.java
@@ -1,45 +1,62 @@
 import java.util.ArrayList;
+import java.util.List;
 
 public class Camp {
-	/**
-	 * Attributes for Camp
-	 */
-	private String _campName;
-	/**
-	 * Name of the camp
-	 */
-	private String _dates;
-	/**
-	 * Dates of the camp
-	 */
-	private String _registrationClosingDate;
-	/**
-	 * Closing date for registration
-	 */
-	private java.util.ArrayList<String> _attendees;
-	/**
-	 * List of attendees
-	 */
-	private java.util.ArrayList<String> _committeeMembers;
+    private String campName;
+    private String dates;
+    private String registrationClosingDate;
+    private String userGroup;  // Own school or whole NTU
+    private String location;
+    private int totalSlots;
+    private int campCommitteeSlots; // Max 10
+    private String description;
+    private Staff staffInCharge;  // Automatically tied to the staff who created it
 
-	/**
-	 * Constructor
-	 */
-	public Camp(String aCampName, String aDates, String aRegistrationClosingDate) {
-		throw new UnsupportedOperationException();
-	}
+    private List<Student> attendees;  // List of students registered for the camp as attendees
+    private List<CampCommitteeMember> committeeMembers;  // List of students registered for the camp as committee
+    
+    private List<Camp> listOfCamps; // List of camps created by staffs
 
-	/**
-	 * Method to add attendee
-	 */
-	public void addAttendee(String aUserID) {
-		throw new UnsupportedOperationException();
-	}
+    public Camp(String campName, String dates, String registrationClosingDate, String userGroup, String location,
+                int totalSlots, int campCommitteeSlots, String description, Staff staffInCharge) {
+        this.campName = campName;
+        this.dates = dates;
+        this.registrationClosingDate = registrationClosingDate;
+        this.userGroup = userGroup;
+        this.location = location;
+        this.totalSlots = totalSlots;
+        this.campCommitteeSlots = campCommitteeSlots;
+        this.description = description;
+        this.staffInCharge = staffInCharge;
 
-	/**
-	 * Method to remove attendee
-	 */
-	public void removeAttendee(String aUserID) {
-		throw new UnsupportedOperationException();
-	}
+        this.attendees = new ArrayList<>();
+        this.committeeMembers = new ArrayList<>();
+    }
+
+    // Getters and setters for the camp attributes
+    
+    public void addCamp(Camp camp) {
+    	listOfCamps.add(camp);
+    }
+
+    public void addAttendee(Student student) {
+        attendees.add(student);
+    }
+
+    public void addCommitteeMember(CampCommitteeMember committeeMember) {
+        committeeMembers.add(committeeMember);
+    }
+
+    public List<Camp> getListOfCamps(){
+    	return listOfCamps;
+    }
+    
+    public List<Student> getAttendees() {
+        return attendees;
+    }
+
+    public List<CampCommitteeMember> getCommitteeMembers() {
+        return committeeMembers;
+    }
 }
+
