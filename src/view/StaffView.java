04c95aec63ea1acf9975354ec47a522c4022b7d6
James Lim <99797532+Limjrj@users.noreply.github.com>
2023-11-17 17:59:22 +0800

    updated staffview
    fixed date errors

--- src/view/StaffView.java
+++ src/view/StaffView.java
@@ -3,6 +3,7 @@ package src.view;
 import java.util.Scanner;
 import src.basic.Staff;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -43,27 +44,33 @@ public class StaffView extends MainView {
                         case 1:
                             // Create camp
 							SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy"); // Date formatter
-							System.out.println("Enter camp name: ");
-							String campName = sc.nextLine();
-							System.out.println("Enter start date (dd-MM-yyyy): ");
-							String startDateStr = sc.nextLine();
-							Date startDate = sdf.parse(startDateStr);
-							System.out.println("Enter end date (dd-MM-yyyy): ");
-							String endDateStr = sc.nextLine();
-							Date endDate = sdf.parse(endDateStr);
-							System.out.println("Enter registration closing date (dd-MM-yyyy): ");
-							String registrationCloseDateStr = sc.nextLine();
-							Date registrationCloseDate = sdf.parse(registrationCloseDateStr);
-							System.out.println("Enter user group: ");
-							String userGroup = sc.nextLine();
-							System.out.println("Enter location: ");
-							String location = sc.nextLine();
-							System.out.println("Enter total slots: ");
-							int totalSlots = sc.nextInt();
-							System.out.println("Enter description: ");
-							String description = sc.nextLine();
-							Staff.createCamp(campName, startDate, endDate, registrationCloseDate, userGroup, 
-											 location, totalSlots, description);
+							try{
+								System.out.println("Enter camp name: ");
+								String campName = sc.nextLine();
+								System.out.println("Enter start date (dd-MM-yyyy): ");
+								String startDateStr = sc.nextLine();
+								Date startDate = sdf.parse(startDateStr);
+								System.out.println("Enter end date (dd-MM-yyyy): ");
+								String endDateStr = sc.nextLine();
+								Date endDate = sdf.parse(endDateStr);
+								System.out.println("Enter registration closing date (dd-MM-yyyy): ");
+								String registrationCloseDateStr = sc.nextLine();
+								Date registrationCloseDate = sdf.parse(registrationCloseDateStr);
+								System.out.println("Enter user group: ");
+								String userGroup = sc.nextLine();
+								System.out.println("Enter location: ");
+								String location = sc.nextLine();
+								System.out.println("Enter total slots: ");
+								int totalSlots = sc.nextInt();
+								System.out.println("Enter description: ");
+								String description = sc.nextLine();
+								Staff.createCamp(campName, startDate, endDate, registrationCloseDate, userGroup, 
+												location, totalSlots, description);
+							} catch (ParseException e){
+								// Handle parsing exception
+								System.out.println("Invalid date format. Please enter the date in the format dd-MM-yyyy.");
+								e.printStackTrace(); // You might want to log the exception or handle it appropriately
+							}
                             break;
 
                         case 2:
