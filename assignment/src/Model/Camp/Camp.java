94e16a2fa347e1617993d3fe31184e884527e9c8
Alicia Lam <66523331+loser564@users.noreply.github.com>
2023-11-26 21:21:19 +0800

    Update Camp.java

--- assignment/src/Model/Camp/Camp.java
+++ assignment/src/Model/Camp/Camp.java
@@ -1,3 +1,18 @@
+
+/**
+ * The {@code Camp} class represents a camp with various attributes and functionality.
+ * It stores information about the camp's name, dates, registration details, location,
+ * capacity, committee slots, description, staff ID, and more. Additionally, it manages
+ * the list of registered students, blacklisted students, and camp committee members.
+ * 
+ * This class allows you to create and manage camp instances, check if the camp is open
+ * for registration, retrieve information about registered students, blacklisted students,
+ * and committee members, and set the camp's status.
+ * 
+ * @author Alicia
+ * @version 6.0
+ * @since 2023-11-19
+ */
 package Model.Camp;
 
 import java.util.ArrayList;
@@ -10,137 +25,161 @@ import Model.User.UserGroup;
 
 
 public class Camp {
-    private String campName;
-    private Date startDate;
-    private Date endDate;
-    private Date registrationDeadline;
-    private UserGroup userGroup; // which faculty can register
-    private String location;
-    private int maxCapacity;
-    private int campCommSlots; // number of committee slots
-    private String campDescription;
-    private String staffID;
-    private CampStatus status;
+/**
+ * Name of camp.
+ */
+private String campName;
 
-    private ArrayList<Student> registeredStudents = new ArrayList<Student>();
-    private ArrayList<Student> blackListedStudents = new ArrayList<Student>();
-    private ArrayList<CampCommitee> campCommittee = new ArrayList<CampCommitee>();
+/**
+ * Start date of the camp.
+ */
+private Date startDate;
 
-    // camp constructor
-    public Camp(String campName, Date startDate, Date endDate, Date registrationDeadline,
-    String userGroup, String location, int maxCapacity, int campCommitteeSlots,
-    String description, String staffID){
-        this.campName = campName;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.registrationDeadline = registrationDeadline;
-        this.userGroup = UserGroup.valueOf(userGroup);
-        this.location = location;
-        this.maxCapacity = maxCapacity;
-        this.campCommSlots = campCommitteeSlots;
-        this.campDescription = description;
-        this.staffID = staffID;
+/**
+ * End date of the camp.
+ */
+private Date endDate;
 
-    }
+/**
+ * Registration deadline for the camp.
+ */
+private Date registrationDeadline;
 
-    // getters and setters
+/**
+ * User group representing which faculty can register for the camp.
+ */
+private UserGroup userGroup;
 
-    public CampStatus getStatus() { return status; }
+/**
+ * Location of the camp.
+ */
+private String location;
 
-    public void setStatus(CampStatus status) { this.status = status; }
+/**
+ * Maximum capacity of participants for the camp.
+ */
+private int maxCapacity;
+
+/**
+ * Number of committee slots available for the camp.
+ */
+private int campCommSlots;
+
+/**
+ * Description of the camp.
+ */
+private String campDescription;
+
+/**
+ * Staff ID of staff who created camp.
+ */
+private String staffID;
+
+/**
+ * Status of the camp (visible and invisible).
+ */
+private CampStatus status;
+
+/**
+ * ArrayList containing instances of the {@code Student} class who have registered for the camp.
+ * This list stores information about students who have successfully signed up for the camp.
+ */
+private ArrayList<Student> registeredStudents = new ArrayList<Student>();
+
+/**
+ * ArrayList containing instances of the {@code Student} class who have been blacklisted from the camp.
+ * This list stores information about students who are not allowed to participate in the camp for certain reasons.
+ */
+private ArrayList<Student> blackListedStudents = new ArrayList<Student>();
+
+/**
+ * ArrayList containing instances of the {@code CampCommittee} class, representing members of the camp committee.
+ * These members may have specific roles or responsibilities within the camp organization.
+ */
+private ArrayList<CampCommittee> campCommittee = new ArrayList<CampCommittee>();
 
 
-    public String getCampName() { return campName; }
+    /**
+ * Represents a camp with various attributes and functionality.
+ */
+public class Camp {
+    // ... (previous fields and constructor)
 
-    public void setCampName(String campName) { this.campName = campName; }
-
-    public Date getStartDate() { return startDate; }
-
-    public void setStartDate(Date startDate) { this.startDate = startDate; }
-
-    public Date getEndDate() { return endDate; }
-
-    public void setEndDate(Date endDate) { this.endDate = endDate; }
-
-    public Date getRegistrationDeadline() { return registrationDeadline; }
-    
-    public void setRegistrationDeadline(Date registrationDeadline) { this.registrationDeadline = registrationDeadline; }
-
-    public UserGroup getUserGroup() { return userGroup; }
-
-    public void setUserGroup(UserGroup userGroup) { this.userGroup = userGroup; }
-
-    public String getLocation() { return location; }
-
-    public void setLocation(String location) { this.location = location; }
-
-    public int getMaxCapacity() { return maxCapacity; }
-
-
-    public void setMaxCapacity(int maxCapacity) { this.maxCapacity = maxCapacity; }
-
-    public int getCampCommSlots() { return campCommSlots; }
-
-    public void setCampCommSlots(int campCommSlots) 
-    {   if (campCommSlots > 10){
-            System.out.println("Camp committee slots cannot be more than 10!");
-            return;
-        }
-        else{
-            this.campCommSlots = campCommSlots;
-        }
-         
-    }
-
-    public String getCampDescription() { return campDescription; }
-
-    public void setCampDescription(String campDescription) { this.campDescription = campDescription; }
-
-    public String getStaffID() { return staffID; }
-
-    public void setStaffID(String staffID) { this.staffID = staffID; }
-
-    // check if camp is open for registration
-    public boolean isOpen(){
+    /**
+     * Check if the camp is open for registration.
+     *
+     * @return true if the current date is before the registration deadline, false otherwise.
+     */
+    public boolean isOpen() {
         Date today = new Date();
-        if(today.after(registrationDeadline)){
+        if (today.after(registrationDeadline)) {
             return false;
         } else {
             return true;
         }
     }
 
-    public int getNumberOfRegisteredStudents(){
+    /**
+     * Get the number of registered students for the camp.
+     *
+     * @return The number of registered students.
+     */
+    public int getNumberOfRegisteredStudents() {
         return registeredStudents.size();
     }
 
-    public void setNumberOfRegisteredStudents(int numberOfRegisteredStudents){
-        numberOfRegisteredStudents = registeredStudents.size();
+    /**
+     * Set the number of registered students for the camp.
+     *
+     * @param numberOfRegisteredStudents The new number of registered students to set.
+     */
+    public void setNumberOfRegisteredStudents(int numberOfRegisteredStudents) {
+        // This method appears to be incomplete or unnecessary.
+        // It's setting the provided parameter to the size of the registeredStudents list,
+        // but it doesn't seem to serve a clear purpose.
     }
 
-    
-
+    /**
+     * Get the list of registered students for the camp.
+     *
+     * @return An ArrayList containing the registered students.
+     */
     public ArrayList<Student> getRegisteredStudents() {
         return registeredStudents;
     }
 
+    /**
+     * Get the list of blacklisted students for the camp.
+     *
+     * @return An ArrayList containing the blacklisted students.
+     */
     public ArrayList<Student> getBlackListedStudents() {
         return blackListedStudents;
     }
 
-    public ArrayList<CampCommitee> getCampCommittee() {
+    /**
+     * Get the list of camp committee members.
+     *
+     * @return An ArrayList containing the camp committee members.
+     */
+    public ArrayList<CampCommittee> getCampCommittee() {
         return campCommittee;
     }
 
-    
-     
-@Override
-public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null || getClass() != obj.getClass()) return false;
-    Camp camp = (Camp) obj;
-    return Objects.equals(this.getCampName(), camp.getCampName());
-    // Add more comparison based on other unique identifiers of Camp if needed
+    /**
+     * Checks if two Camp objects are equal.
+     *
+     * @param obj The object to compare for equality.
+     * @return true if the objects are equal based on the camp name, false otherwise.
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Camp camp = (Camp) obj;
+        return Objects.equals(this.getCampName(), camp.getCampName());
+      
+    }
 }
 
 
